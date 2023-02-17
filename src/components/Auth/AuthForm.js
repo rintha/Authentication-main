699a5daaed9f38d7d87b567bd8dd2fbca3c009cf
rintha <rintharithu@gmail.com>
2023-02-17 10:58:07 +0530

    userlogin added

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -22,38 +22,48 @@ const AuthForm = () => {
     // optional: Add validation
 
     setIsLoading(true);
+    let url;
     if (isLogin) {
-      
+      url ="https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCV5PCOgc58VMOOPtzVDaG0A-8gH8GwMzc"; 
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCV5PCOgc58VMOOPtzVDaG0A-8gH8GwMzc",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
-        setIsLoading(false);
-        if (res.ok) {
-          
-        } else {
-          return res.json().then((data) => {
-            let errorMessage = "Authentication failed!";
-            if (data && data.error && data.error.message) {
-              errorMessage = data.error.message;
-            }
-            alert(errorMessage);
-          
-          });
-        }
-      });
+      url="https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCV5PCOgc58VMOOPtzVDaG0A-8gH8GwMzc"
     }
+    fetch(
+      url,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+        
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = "Authentication failed!";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+        
+          throw new Error(errorMessage);
+        
+        });
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
   };
 
   return (
